19583734cf9d6b5da303d4a9e71502825bd941dd
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-04-02 13:59:23 +0200

    style: add semicolon

--- Conditional_operators/task_1/index.js
+++ Conditional_operators/task_1/index.js
@@ -1,8 +1,8 @@
 let name = prompt("What is the official name for JavaScript?");
 
 if(name == ECMAScript){
-    alert("You're right!")
+    alert("You're right!");
 }
 else{
-    alert("Don't know? It's ECMAScript!")
+    alert("Don't know? It's ECMAScript!");
 }
