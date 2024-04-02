fc95ca045fedfd5b523bd701910128a871c728a0
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-04-02 14:11:44 +0200

    add task_3

---
+++ Conditional_operators/task_3/index.js
@@ -0,0 +1,13 @@
+//rewrite if-else with "?" conditional operator
+
+/*let result;
+
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}*/
+
+let result;
+
+(a + b < 4) ? "A little" : "A lot";
