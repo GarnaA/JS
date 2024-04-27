92e16d6cee122fd68c967ef4cf247a4b32982c40
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-04-27 15:30:25 +0200

    feat: add task_7

---
+++ date_and_time/task_7/index.js
@@ -0,0 +1,23 @@
+function getSecondsToTomorrow() {
+    let now = new Date();
+  
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+  
+    let diff = tomorrow - now;
+    return Math.round(diff / 1000);
+}
+
+console.log(getSecondsToTomorrow());
+
+/*
+function getSecondsToTomorrow() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let totalSecondsToday = (hour * 60 + minutes) * 60 + seconds;
+    let totalSecondsInADay = 86400;
+    
+    return totalSecondsInADay - totalSecondsToday;
+}
+*/
