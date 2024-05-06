73f7dc93a908184b17f4f37387fd7cf0cee0a022
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-05-06 19:23:28 +0200

    style: modify format

--- tasks/03-get-first-element.js
+++ tasks/03-get-first-element.js
@@ -1,3 +1,11 @@
+/*
+* Task description: Write a JavaScript function to get the first element 
+of an array. Passing the parameter 'n' will return the first 'n' elements of the array
+* Expected result: 7, [], [7, 9, 0], [7, 9, 0, -2], []
+* @param {Array, number} arr - the input array, n - the number of elements of the array
+* @returns {Array}
+*/
+
 function first(arr, n){
     let arr2 = []
     if(n > 0 && n <= arr.length){
