d2d08a2df250ba6b32cda8ae5cbbcb723c402463
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-05-06 19:18:31 +0200

    style: modify format

--- tasks/02-clone-array.js
+++ tasks/02-clone-array.js
@@ -1,3 +1,10 @@
+/*
+* Tsk description: Write a JavaScript function to clone an array.
+* Expected result: [1, 2, 4, 0], [1, 2, [4, 0]]
+* @param {Array} arr - array of numbers
+* @returns{Array}
+*/
+
 function arrayClone(arr){
     let clonedArr = [];
     for(let i = 0; i < arr.length; i++){
