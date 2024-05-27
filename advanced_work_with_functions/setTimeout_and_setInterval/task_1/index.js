37d6b4562fe538639485892ab41d7eb612c4103f
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-05-27 11:03:16 +0200

    feat: add task_1

---
+++ advanced_work_with_functions/setTimeout_and_setInterval/task_1/index.js
@@ -0,0 +1,12 @@
+function printNumbers(from, to) {
+  let current = from;
+  let timerId = setInterval( function() {
+    console.log(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
+printNumbers(5, 10);
