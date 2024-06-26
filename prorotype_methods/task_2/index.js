2b7f543734cafbf02d4f86d1e95fcc528aade733
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-06-26 14:25:46 +0200

    feat: add task_2

---
+++ prorotype_methods/task_2/index.js
@@ -0,0 +1,13 @@
+function Rabbit(name) {
+  this.name = name;
+}
+Rabbit.prototype.sayHi = function() {
+  console.log(this.name);
+};
+  
+let rabbit = new Rabbit("Rabbit");
+
+rabbit.sayHi(); //Rabbit
+Rabbit.prototype.sayHi(); //undefined
+Object.getPrototypeOf(rabbit).sayHi(); //undefined
+rabbit.__proto__.sayHi(); //undefined
