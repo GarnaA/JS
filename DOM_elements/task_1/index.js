f92e7488370427aeba8ae6982df468a0a0960851
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-07-04 22:35:31 +0200

    feat: add  task_1

---
+++ DOM_elements/task_1/index.js
@@ -0,0 +1,29 @@
+//Html page: 
+
+/* 
+<html>
+<body>
+  <div>Пользователи:</div>
+  <ul>
+    <li>Джон</li>
+    <li>Пит</li>
+  </ul>
+</body>
+</html>
+*/
+
+//to get <div>
+document.body.firstElementChild
+
+document.body.children[0]
+
+document.body.childNodes[1]
+
+//to get <ul>
+document.body.lastElementChild
+
+document.body.children[1]
+
+//to get second <li>
+document.body.lastElementChild.lastElementChild
+
